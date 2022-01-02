cc529c4dee56ae1b75bff07338f3e32e213952f3
공준배 <ballgaveship@gmail.com>
2022-01-03 00:29:25 +0900

    초기 셋팅 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,22 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.6.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id ("org.jetbrains.kotlin.plugin.allopen") version "1.6.0"
+    id ("org.jetbrains.kotlin.plugin.noarg") version "1.6.0"
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.spring") version "1.6.0"
     kotlin("plugin.jpa") version "1.6.0"
     java
+}
+
+noArg {
+    annotation("javax.persistence.Entity")
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
 }
 
 java {
@@ -26,15 +38,37 @@ allprojects {
 subprojects {
     apply(plugin = "io.spring.dependency-management")
     apply(plugin = "kotlin")
+    if (name == "menu"
+        || name == "order"
+        || name == "user") {
+        apply(plugin = "org.springframework.boot")
+        apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+
+        dependencies {
+            implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+            implementation("org.jetbrains.kotlin:kotlin-reflect")
+            implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+            implementation("io.github.microutils:kotlin-logging")
+        }
+
+        tasks.jar {
+            enabled = true
+        }
+        tasks.forEach {
+            if (it.name == "bootJar") {
+                it.enabled = false
+            }
+        }
+    }
 
     dependencyManagement {
         dependencies {
             imports {
                 mavenBom("org.springframework.cloud:spring-cloud-dependencies:2021.0.0")
             }
+            dependency("io.github.microutils:kotlin-logging:1.12.5")
         }
     }
-
 
     tasks.withType<KotlinCompile> {
         kotlinOptions {
--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -1,22 +1,6 @@
-apply(plugin = "org.springframework.boot")
-apply(plugin = "org.jetbrains.kotlin.plugin.spring")
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.flywaydb:flyway-core")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
-}
-
-tasks.jar {
-    enabled = true
-}
-
-tasks.forEach {
-    if (it.name == "bootJar") {
-        it.enabled = false
-    }
 }
